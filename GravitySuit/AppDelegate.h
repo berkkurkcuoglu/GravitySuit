09b3da18fa6cac3495be53048f009b3e4e70e99c
berk <berk@berks-imac.local>
2017-05-06 19:41:35 -0400

    Initial Commit

---
+++ GravitySuit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GravitySuit
+//
+//  Created by berk on 5/6/17.
+//  Copyright © 2017 berk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GravitySuit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GravitySuit
+//
+//  Created by berk on 5/6/17.
+//  Copyright © 2017 berk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
